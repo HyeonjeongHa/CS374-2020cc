c7feaf867a44469fb77ae47cbada940b371ebb67
template97 <gogodbwngud@naver.com>
2020-06-04 20:48:13 +0900

    little change

--- 2020cc-react/src/components/mainscreen/scheduler.js
+++ 2020cc-react/src/components/mainscreen/scheduler.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 import { Todo } from '../../routes';
 
 class Scheduler extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: this.props.data
-		};
-	}
 
 	render() {
 		const dailyView = (
