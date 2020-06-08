a9512b222fdb5d31f0d2741b8bb3b3fbc21286a6
minji971010 <minji971010@kaist.ac.kr>
2020-06-08 15:21:30 +0900

    no message

--- 2020cc-react/src/components/mainscreen/coworker.js
+++ 2020cc-react/src/components/mainscreen/coworker.js
@@ -61,7 +61,9 @@ class Coworker extends Component {
 	    return (
 			<div className="coworker" style={style}>
 				{this.state.flag 
-					? this.state.coworkerList.map(data => (<Person progress={80} handler={this.props.handler} name={data.name} id={data.id} teamName={data.teamName} position={data.position} />))
+					? this.state.coworkerList.map(data => (
+                    <Person progress={80} handler={this.props.handler} name={data.name} id={data.id} teamName={data.teamName} position={data.position} />
+                    ))
                     : ( <div/>
                 )}
 	   		</div>
