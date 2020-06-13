f23f1f017c8470edd1251ac8acc339ba3f74c475
minji971010 <minji971010@kaist.ac.kr>
2020-06-13 15:54:03 +0900

    show message when no schedule

--- 2020cc-react/src/components/mainscreen/todoList.js
+++ 2020cc-react/src/components/mainscreen/todoList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { TodoInfo} from '..';
+import { TodoInfo } from '..';
+import '../../mainscreen.css';
 
 class TodoList extends Component {
 
@@ -9,30 +10,48 @@ class TodoList extends Component {
           padding: '25px',
           margin: '15px',
         },
-        TodoList : this.props.data
+        TodoList : this.props.data,
       };
+
+
 
     render() {
         const { data, onUpdate, onRemove, onLikey } = this.props;
         // console.log("[todoList.js] render() data!!!!!!!", data);
         // var heartFlag;
         // if (data.likey["null"] == "1") {
-        //     heartFlag = false;
         // } else {
         //     if (data.likey[this.props.clickID] == "1") {
         //         heartFlag = true;
         //     }
         //     else {
-        //         heartFlag = false;
         //     }
         // }
-        return (
-            <div>
+        console.log("this.props.data", this.props.data);
+        var myNullFlag = true;
+        if (data.length > 0) {
+            myNullFlag = false;
+        } 
+        const myTodoInfo = (
                 <ul>
                 {data.map(data  => (
                     <TodoInfo TodoList = {this.props.data} loginID={this.props.loginID} data={data} onUpdate={onUpdate} onRemove={onRemove} onLikey={onLikey} isCoworker={this.props.isCoworker}/>
                 ))}
                 </ul>
+        )
+        const myNullInfo = (
+            <div className="nullSign">
+                
+                There is no Schedule to show !
+                
+            </div>
+        )
+
+        console.log("myNullFlag", myNullFlag);
+
+        return (
+            <div>
+            {myNullFlag ? myNullInfo : myTodoInfo}
             </div>
         );
     }
